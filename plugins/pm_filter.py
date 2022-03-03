ac16a1afea5461a034f589de94e05385b427de8e
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-03-03 21:14:27 +0530

    Checking

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -787,6 +787,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('𝙴𝚇𝚃𝚁𝙰', callback_data='extra'),
             InlineKeyboardButton("𝚅𝙸𝙳𝙴𝙾", callback_data='video')
             ],[
+            InlineKeyboardButton('⚚ ✶ 𝙱𝙰𝙲𝙺 ✶ ⚚', callback_data='start'),         
+            InlineKeyboardButton("𝗡𝗲𝘅𝘁 ➡️", callback_data='next')     
+            ],[ 
             InlineKeyboardButton('𝙿𝙸𝙽', callback_data='pin'), 
             InlineKeyboardButton('𝙿𝙰𝚂𝚃𝙴', callback_data='pastes'),
             InlineKeyboardButton("𝙸𝙼𝙰𝙶𝙴", callback_data='image')
