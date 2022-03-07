2f1ff43c8f899436a5d5ad72086fec52b4f18c35
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-03-07 20:54:17 +0530

    Bug😵

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,6 +53,9 @@ async def start(client, message):
             InlineKeyboardButton(' 𝙾𝚠𝚗𝚎𝚛 ', url='https://t.me/albintko'),
             InlineKeyboardButton(' 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ', url='https://t.me/+AMHw_K1wvOM3MTU9')
         ]]
+        await message.reply_chat_action("typing")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEOha9iJh88DjhbAAHMnlBkOb5DsjaPcNwAAgMDAAI6ZolXwVJ2tZvuCnQjBA") 
+        await asyncio.sleep(1) await m.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
         await message.msg.delete,   
@@ -102,6 +105,9 @@ async def start(client, message):
             InlineKeyboardButton(' 𝙾𝚠𝚗𝚎𝚛 ', url='https://t.me/albintko'),
             InlineKeyboardButton(' 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ', url='https://t.me/+AMHw_K1wvOM3MTU9')
         ]]
+        await message.reply_chat_action("typing")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEOha9iJh88DjhbAAHMnlBkOb5DsjaPcNwAAgMDAAI6ZolXwVJ2tZvuCnQjBA") 
+        await asyncio.sleep(1) await m.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
             video=random.choice(PICS),
